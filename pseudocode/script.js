f8e5941f2f475d9a2eab7d9de0ae7af1ef7b494b
Student_MA109 <student_ma109@Student-MA109.local>
2020-03-12 12:40:33 -0400

    First Draft

---
+++ pseudocode/script.js
@@ -0,0 +1,20 @@
+
+
+// on random the a player is going to be promted to start the game;
+    // a function that randomly returns a number 0 to 1 
+
+
+//  each player has a button that alows them to choose what row they want to drop down a chip
+    
+// when a chip is droped it will match with given grid 
+
+//     each row is going to be filled up once a button is clicked the grid is 7X6
+
+//     the occupied space is going to remain but the colors will rotate 
+
+
+
+
+// if the player decides to quit then the page will refresh 
+
+
