96d52b386cde9c1038bbfd3c3b45dde2c7df37ab
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-16 12:55:17 -0400

    setting up board

--- app.js
+++ app.js
@@ -17,4 +17,13 @@ const colors = {
 let switchTurn = false
 let currPlayer = p1
 
-const board = [] // need a refrence board 7X6
+const board = [
+  /* 
+    0      1      2     3     4     5      6    (cols) */
+  [null, null, null, null, null, null, null], // row 0
+  [null, null, null, null, null, null, null], // row 1
+  [null, null, null, null, null, null, null], // row 2
+  [null, null, null, null, null, null, null], // row 3
+  [null, null, null, null, null, null, null], // row 4
+  [null, null, null, null, null, null, null]  // row 5
+  ]
