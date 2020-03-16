ff8d209d1c9e830b7337802af31a80312ea931df
Student_MA109 <student_ma109@Student-MA109.lan1>
2020-03-16 11:57:07 -0400

    pseudocode

--- pseudocode/script.js
+++ pseudocode/script.js
@@ -6,15 +6,30 @@
 
 //  each player has a button that alows them to choose what row they want to drop down a chip
     
-// when a chip is droped it will match with given grid 
-
-//     each row is going to be filled up once a button is clicked the grid is 7X6
-
-//     the occupied space is going to remain but the colors will rotate 
+// start woring form the bottom of the grid 
+// if the bottom is full then each token can be stacked 
 
 
+//  each row is going to be filled up once a button is clicked the grid is 7X6
 
 
-// if the player decides to quit then the page will refresh 
+//the occupied space is going to remain but the colors will rotate 
+
+
+//to see differnt winning positions
+//if the same player is vertically stacked then that player wins
+//test if the upwards on the grid to see if the same tokens  are === to the same player
+
+
+// same for ,horizontal 
+// check to see if the near by tokens are === to same player 
+
+// for diagonal end point is to check every range from the top right to bottom left and the top left to the bottom right 
+
+
+
+
+// if the player decides to quit then then the othe player wins 
+// if any palyer wishes to restart the page will refresh  
 
 
